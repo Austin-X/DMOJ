73f5e765aa54ae2188850bc854055496bda58ef6
Austin-X <32133198+Austin-X@users.noreply.github.com>
2019-05-18 22:52:44 -0400

    Create Quine.java

---
+++ Uncategorized/Quine.java
@@ -0,0 +1,1 @@
+public class Quine{public static void main(String[]a){String s="public class Quine{public static void main(String[]a){String s=%c%s%1$c;System.out.printf(s,34,s);}}";System.out.printf(s,34,s);}}
