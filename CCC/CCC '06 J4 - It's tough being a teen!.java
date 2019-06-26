a1962c7a207312213bf062f521e0176b91b00201
Austin-X <32133198+Austin-X@users.noreply.github.com>
2019-06-25 20:27:36 -0400

    Update CCC '06 J4 - It's tough being a teen!.java

--- CCC/CCC '06 J4 - It's tough being a teen!.java
+++ CCC/CCC '06 J4 - It's tough being a teen!.java
@@ -2,65 +2,69 @@ import java.io.*;
 import java.util.*;
 
 public class ItsToughBeingATeen {
+	static int getMinValue(ArrayList<Integer>[] adj, boolean[] visited) {
+		int min = -1; boolean contains;
+		for (int i = 1; i <= 7; i ++) {
+			contains = false;
+			for (int j = 1; j <= 7; j ++) {
+				if (!adj[j].contains(i)) {
+					contains = true;
+				} else {
+					contains = false; break;
+				}
+			}
+			if (contains && !visited[i]) {
+				min = i; break;
+			}
+		}
+		
+		return min;
+	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		boolean[] visited = new boolean[8];
+		ArrayList<Integer> out = new ArrayList<Integer>();
 		@SuppressWarnings("unchecked")
 		ArrayList<Integer>[] adj = new ArrayList[8];
-		
 		for (int i = 0; i < 8; i ++) adj[i] = new ArrayList<Integer>();
-		adj[7].add(1);
-		adj[4].add(1);
-		adj[1].add(2);
-		adj[4].add(3);
-		adj[5].add(3);
+		
+		// This means that chore number 1 comes before chore number 7
+		adj[1].add(7);
+		adj[1].add(4);
+		adj[2].add(1);
+		adj[3].add(4);
+		adj[3].add(5);
 		
 		while (true) {
 			int x = Integer.parseInt(br.readLine());
 			int y = Integer.parseInt(br.readLine());
 			
 			if (x == 0 && y == 0) break;
-			
-			adj[y].add(x);
+			adj[x].add(y);
 		}
 		
-		ArrayList<Integer> out = new ArrayList<Integer>();
+		int min = getMinValue(adj, visited);
 		
-		int min = -1;
-		for (int i = 1; i < 8; i ++) {
-			if (adj[i].isEmpty()) {
-				min = i; break;
-			}
-		}
-		
-		boolean[] visited = new boolean[8];
 		Queue<Integer> queue = new LinkedList<Integer>();
 		queue.add(min);
 		
-		while (!queue.isEmpty()) {
+		while(!queue.isEmpty()) {
 			int cur = queue.poll();
-			visited[cur] = true;
 			out.add(cur);
-			for (int i = 1; i < 8; i ++) {
-				for (int j = 0; j < adj[i].size(); j ++) {
-					if (adj[i].get(j) == cur) {
-						adj[i].remove(j);
-					}
-				}
-			}
+			visited[cur] = true;
 			
-		    min = -1;
-			for (int i = 1; i < 8; i ++) {
-				if (!visited[i] && adj[i].isEmpty()) {
-					min = i; break;
-				}
+			for (int i = 0; i < adj[cur].size();) {
+				adj[cur].remove(i);
 			}
-			
-			if (min != -1) queue.add(min);
+
+		    min = getMinValue(adj, visited);
+		    
+		    if (min != -1) queue.add(min);
 		}
 		
-		if (out.size() == 7)
+		if (out.size() == 7) {
 			for (int x : out) System.out.print(x + " ");
-		else System.out.println("Cannot complete these tasks. Going to bed.");
+		} else System.out.println("Cannot complete these tasks. Going to bed.");
 	}
 }
