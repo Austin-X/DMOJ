13775f39a0d05c0888f9abaa9e946320a3f9118f
Austin-X <32133198+Austin-X@users.noreply.github.com>
2019-06-28 09:06:05 -0400

    Update Is it a Tree?.java

--- Seasonal/Is it a Tree?.java
+++ Seasonal/Is it a Tree?.java
@@ -6,44 +6,44 @@ public class IsItATree {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
+		boolean tree = true;
+		
 		@SuppressWarnings("unchecked")
 		ArrayList<Integer>[] adj = new ArrayList[5];
-		for (int i = 0; i < 5; i ++) adj[i] = new ArrayList<Integer>();
 		
+		for (int i = 0; i < 5; i ++) adj[i] = new ArrayList<Integer>();
 		for (int i = 1; i <= 4; i ++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= 4; j ++) {
 				if (Integer.parseInt(st.nextToken()) == 1) adj[i].add(j);
 			}
 		}
-		
-		Queue<Integer> queue = new LinkedList<Integer>();
-		queue.add(1);
+
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(1);
 		boolean[] visited = new boolean[5];
 		visited[1] = true;
-		int[] parent = new int[5];
-		Arrays.fill(parent, -1);
+		int[] parent = new int[5]; 
 		
-		while (!queue.isEmpty()) {
-			int cur = queue.poll();
-		
+		while (!stack.empty()) {
+			int cur = stack.pop();
 			for (int x : adj[cur]) {
 				if (!visited[x]) {
 					visited[x] = true;
-					queue.add(x);
+					stack.push(x);
 					parent[x] = cur;
 				} else if (parent[cur] != x) {
-					System.out.println("No"); System.exit(0);
+					tree = false; stack.clear(); break;
 				}
 			}
 		}
-
+		
 		for (int i = 1; i <= 4; i ++) {
 			if (!visited[i]) {
-				System.out.println("No"); System.exit(0);
+				tree = false; break;
 			}
 		}
 		
-		System.out.println("Yes");
+		System.out.println(tree ? "Yes" : "No");
 	}
 }
