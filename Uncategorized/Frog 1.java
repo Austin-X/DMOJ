06d6ccefef8ad808f6da0f96c2725f53f9b24c25
Austin-X <32133198+Austin-X@users.noreply.github.com>
2019-07-11 20:13:42 -0400

    Update Frog 1.java

--- Uncategorized/Frog 1.java
+++ Uncategorized/Frog 1.java
@@ -2,13 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class Frog1 {
-	static int[] pfs;
+	static int[] dp;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
 		int n = Integer.parseInt(br.readLine());
 		int[] heights = new int[n];
-		pfs = new int[n];
+		dp = new int[n];
 	
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i ++) heights[i] = Integer.parseInt(st.nextToken());
@@ -17,11 +17,11 @@ public class Frog1 {
 	}
 	
 	static int solve(int[] h, int n) {
-		pfs[0] = 0; pfs[1] = Math.abs(h[1] - h[0]);
+		dp[0] = 0; dp[1] = Math.abs(h[1] - h[0]);
 		for (int i = 2; i < n; i ++) {
-			pfs[i] = Math.min(pfs[i - 1] + Math.abs(h[i] - h[i - 1]), pfs[i - 2] + Math.abs(h[i] - h[i - 2]));
+			dp[i] = Math.min(dp[i - 1] + Math.abs(h[i] - h[i - 1]), dp[i - 2] + Math.abs(h[i] - h[i - 2]));
 		}
 		
-		return pfs[n - 1];
+		return dp[n - 1];
 	}
 }
