440dd6aafe34b07769e0498f86f9fc90f4877184
Austin-X <32133198+Austin-X@users.noreply.github.com>
2019-07-16 22:51:22 -0400

    Create  CCC '00 S4 - Golf.java

---
+++ CCC/CCC '00 S4 - Golf.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Golf {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int distance = Integer.parseInt(br.readLine());
+		int clubs = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[distance + 1];
+		dp[0] = 1;
+		int[] hits = new int[clubs];
+		for (int i = 0; i < clubs; i ++) {
+			hits[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(hits);
+		
+		for (int i = 0; i < clubs; i ++) {
+			int temp = hits[i];
+			
+			while (temp <= distance) {
+				for (int j = distance; j >= 0; j --) {
+					if (j == 0) dp[j + hits[i]] = 1;
+					else if (dp[j] != 0 && j + hits[i] <= distance) dp[j + hits[i]] = dp[j] + 1;
+				}
+				temp += hits[i];
+			}
+		}
+
+		if (dp[distance] != 0) System.out.println("Roberta wins in " + dp[distance] + " strokes.");
+		else System.out.println("Roberta acknowledges defeat.");
+	}
+}
