20e2d3118de3a82da3fc86c508aaa9fcb3047a6a
Austin-X <32133198+Austin-X@users.noreply.github.com>
2019-07-17 09:56:05 -0400

    Update CCC '00 S4 - Golf.java

--- CCC/CCC '00 S4 - Golf.java
+++ CCC/CCC '00 S4 - Golf.java
@@ -9,7 +9,8 @@ public class Golf {
 		int clubs = Integer.parseInt(br.readLine());
 		
 		int[] dp = new int[distance + 1];
-		dp[0] = 1;
+		Arrays.fill(dp, -1);
+		dp[0] = 0;
 		int[] hits = new int[clubs];
 		for (int i = 0; i < clubs; i ++) {
 			hits[i] = Integer.parseInt(br.readLine());
@@ -21,14 +22,13 @@ public class Golf {
 			
 			while (temp <= distance) {
 				for (int j = distance; j >= 0; j --) {
-					if (j == 0) dp[j + hits[i]] = 1;
-					else if (dp[j] != 0 && j + hits[i] <= distance) dp[j + hits[i]] = dp[j] + 1;
+					if (dp[j] != -1 && j + hits[i] <= distance) dp[j + hits[i]] = dp[j] + 1;
 				}
 				temp += hits[i];
 			}
 		}
 
-		if (dp[distance] != 0) System.out.println("Roberta wins in " + dp[distance] + " strokes.");
+		if (dp[distance] != -1) System.out.println("Roberta wins in " + dp[distance] + " strokes.");
 		else System.out.println("Roberta acknowledges defeat.");
 	}
 }
