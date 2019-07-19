ca20763c5871b857d70f218e6abfb6751447b29f
Austin-X <32133198+Austin-X@users.noreply.github.com>
2019-07-19 19:34:44 -0400

    Update DMOPC '14 Contest 5 P3 - Golden Lily.java

--- DMOPC/DMOPC '14 Contest 5 P3 - Golden Lily.java
+++ DMOPC/DMOPC '14 Contest 5 P3 - Golden Lily.java
@@ -8,43 +8,34 @@ public class GoldenLily {
 		
 		int l = Integer.parseInt(st.nextToken());
 		int d = Integer.parseInt(st.nextToken());
-		int[][] cave = new int[d][l];
+		int[][] density = new int[d][l];
 		
 		for (int i = 0; i < d; i ++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < l; j ++) cave[i][j] = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < l; j ++) density[i][j] = Integer.parseInt(st.nextToken());
 		}
 		
 		st = new StringTokenizer(br.readLine());
-		int finalCol = Integer.parseInt(st.nextToken());
-		int finalRow = Integer.parseInt(st.nextToken());
-
+		int col = Integer.parseInt(st.nextToken());
+		int row = Integer.parseInt(st.nextToken());
+		
 		int[][] dp = new int[d][l];
-		dp[0] = Arrays.copyOf(cave[0], l);
-		for (int i = 1; i < d; i ++) {
-			Arrays.fill(dp[i], Integer.MAX_VALUE);
-		}
-
-		for (int i = 1; i < l; i ++) {
-			dp[0][i] += dp[0][i - 1];
-		}
+		dp[0] = Arrays.copyOf(density[0], l);
+		for (int i = 1; i < l; i ++) dp[0][i] += dp[0][i - 1];
 		
 		for (int i = 1; i < d; i ++) {
 			for (int j = l - 1; j >= 0; j --) {
-				if (j == l - 1) dp[i][j] = dp[i - 1][j] + cave[i][j];
+				if (j == l - 1) dp[i][j] = dp[i - 1][j] + density[i][j];
 				else {
-					dp[i][j] = Math.min(dp[i - 1][j], dp[i][j + 1]) + cave[i][j];
+					dp[i][j] = Math.min(dp[i - 1][j], dp[i][j + 1]) + density[i][j];
 				}
 			}
 			
-			for (int j = 0; j < l; j ++) {
-				if (j == 0) dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + cave[i][j]);
-				else {
-					dp[i][j] = Math.min(dp[i][j], Math.min(dp[i - 1][j], dp[i][j - 1]) + cave[i][j]);
-				} 
+			for (int j = 1; j < l; j ++) {
+				dp[i][j] = Math.min(dp[i][j], Math.min(dp[i - 1][j], dp[i][j - 1]) + density[i][j]);
 			}
 		}
-	
-		System.out.println(dp[finalRow][finalCol]);
+		
+		System.out.println(dp[row][col]);
 	}
 }
