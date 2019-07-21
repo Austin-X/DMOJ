655415b0afaff6dc54f088bfd132a894fa0eca34
Austin-X <32133198+Austin-X@users.noreply.github.com>
2019-07-21 12:08:51 -0400

    Update DMOPC '14 Contest 3 P4 - Not Enough Testers!.java

--- DMOPC/DMOPC '14 Contest 3 P4 - Not Enough Testers!.java
+++ DMOPC/DMOPC '14 Contest 3 P4 - Not Enough Testers!.java
@@ -2,33 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class NotEnoughTesters {
-	// All numbers from 1 to 100000 will all have less than or equal to 200 factors, so the dimensions are 200 * 100000.
-	static int[][] factors = new int[201][100001];
-			
+	static int[] factors = new int[100001];
+	@SuppressWarnings("unchecked")
+	static ArrayList<Integer>[] list = new ArrayList[100001];
+	
 	static void precompute() {
+		for (int i = 0; i <= 100000; i ++) list[i] = new ArrayList<Integer>();
+		
 		for (int i = 1; i <= 100000; i ++) {
-			int fctors = 1, cur = i;
-			
-			for (int j = 2; j <= (int)Math.sqrt(i); j ++) {
-				int cn = 1;
-				
-				while (cur % j == 0) {
-					cur /= j;
-					cn ++;
-				}
-				fctors *= cn;
+			for (int j = i; j <= 100000; j += i) {
+				factors[j] ++;
 			}
-			
-			// If cur is greater than 1, it means that the number is prime and can therefore only have 2 factors
-			if (cur > 1) fctors *= 2;
-			
-			factors[fctors][i] = 1;
-		}
-
-		for (int i = 0; i <= 200; i ++) {
-			for (int j = 1; j <= 100000; j ++) {
-				factors[i][j] += factors[i][j - 1];
-			}
+			list[factors[i]].add(i);
 		}
 	}
 	
@@ -43,9 +28,14 @@ public class NotEnoughTesters {
 			int k = Integer.parseInt(st.nextToken());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-
-			if (k > 138) System.out.println(0);
-			else System.out.println(factors[k][b] - factors[k][a - 1]);
+			
+			int low = Collections.binarySearch(list[k], a);
+			int high = Collections.binarySearch(list[k], b);
+			
+			if (low < 0 && high < 0) System.out.println((-high) + low);
+			else if (low < 0 && high >= 0) System.out.println(high + low + 2);
+			else if (low >= 0 && high < 0) System.out.println((-high) - low - 1);
+			else System.out.println(high - low + 1);
 		}
 	}
 }
