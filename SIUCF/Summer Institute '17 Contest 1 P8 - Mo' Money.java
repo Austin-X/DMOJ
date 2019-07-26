3cb4e8dad0309f6dea79eb6bfc37663bfc95d27a
Austin-X <32133198+Austin-X@users.noreply.github.com>
2019-07-26 09:31:10 -0400

    Update Summer Institute '17 Contest 1 P8 - Mo' Money.java

--- SIUCF/Summer Institute '17 Contest 1 P8 - Mo' Money.java
+++ SIUCF/Summer Institute '17 Contest 1 P8 - Mo' Money.java
@@ -1,29 +1,29 @@
 import java.io.*;
+import java.util.*;
 
 public class MoMoney {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] temp = br.readLine().split(" ");
-		int n = Integer.parseInt(temp[0]);
-		int k = Integer.parseInt(temp[1]);
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int[] arr = new int[n + 1];
-		temp = br.readLine().split(" ");
+		int n = Integer.parseInt(st.nextToken());
+		int t = Integer.parseInt(st.nextToken());		
+		int[] dp = new int[t + 1];
+		dp[0] = 1;
 		
-		for (int i = 1; i < n + 1; i ++) arr[i] = Integer.parseInt(temp[i - 1]);
+		int[] arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i ++) arr[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(arr);
 		
-		int[][] dp = new int[n + 1][k + 1];
-		dp[0][0] = 1;
-		
-		for (int i = 1; i < n + 1; i ++) {
-			for (int j = 0; j < k + 1; j ++) {
-				if (j - arr[i] >= 0) {
-					dp[i][j] = dp[i - 1][j - arr[i]] + dp[i - 1][j];
-				} else {
-					dp[i][j] = dp[i - 1][j];
+		for (int i = 0; i < n; i ++) {
+			for (int j = t; j >= arr[i]; j --) {
+				if (dp[j - arr[i]] != 0) {
+					dp[j] += dp[j - arr[i]];
 				}
 			}
 		}
-		System.out.println(dp[n][k]);
+
+		System.out.println(dp[t]);
 	}
 }
