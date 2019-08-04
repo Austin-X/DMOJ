f23c2b3f0539ea612a4ff9179235bbdfc667fdc2
Austin-X <32133198+Austin-X@users.noreply.github.com>
2019-08-04 13:41:11 -0400

    Update CCC '07 S5 - Bowling for Numbers.java

--- CCC/CCC '07 S5 - Bowling for Numbers.java
+++ CCC/CCC '07 S5 - Bowling for Numbers.java
@@ -7,31 +7,30 @@ public class BowlingForNumbers {
 		StringTokenizer st;
 		
 		int T = Integer.parseInt(br.readLine());
+		
 		for (int t = 0; t < T; t ++) {
 			st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
 			int k = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
 			
-			int[] pins = new int[n];
-			for (int i = 0; i < n; i ++) pins[i] = Integer.parseInt(br.readLine());
-			int[][] dp = new int[k + 1][n];
+			int[] pins = new int[n + 1];
+			for (int i = 1; i <= n; i ++) pins[i] = Integer.parseInt(br.readLine());
 			
+			int[][] dp = new int[k + 1][n + 1];
 			for (int i = 1; i <= k; i ++) {
-				for (int j = 0; j < n; j ++) {
+				for (int j = 1; j <= n; j ++) {
 					int score = 0;
 					for (int x = j; x > j - w; x --) {
 						if (x == -1) break;
 						score += pins[x];
 					}
 					
-					if (i > 1 && j >= w) dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j - w] + score);
-					else if (j > 0) dp[i][j] = Math.max(score, dp[i][j - 1]);
-					else dp[i][j] = score;
+					dp[i][j] = j >= w ? Math.max(dp[i][j - 1], dp[i - 1][j - w] + score) : score;
 				}
 			}
 			
-			System.out.println(dp[k][n - 1]);
+			System.out.println(dp[k][n]);
 		}
 	}
 }
