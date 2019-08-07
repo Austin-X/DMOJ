2d6c8b91814010497bbe34c8a0aef7e78ff61c47
Austin-X <32133198+Austin-X@users.noreply.github.com>
2019-08-07 11:36:51 -0400

    Create  Single Source Shortest Path.java

---
+++ Classics/Single Source Shortest Path.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class SingleSourceShortestPath {
+	static class Node {
+		int vertex, weight;
+		
+		Node (int vertex, int weight) {
+			this.vertex = vertex; 
+			this.weight = weight;
+		}
+ 	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		@SuppressWarnings("unchecked")
+		ArrayList<Node>[] adj = new ArrayList[n + 1];
+		for (int i = 0; i <= n; i ++) adj[i] = new ArrayList<Node>();
+		
+		for (int i = 0; i < m; i ++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			
+			adj[u].add(new Node(v, w));
+			adj[v].add(new Node(u, w));
+		}
+		
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(new Node(1, 0));
+
+		int[] dis = new int[n + 1];
+		Arrays.fill(dis, Integer.MAX_VALUE);
+		dis[1] = 0;
+		
+		while (!q.isEmpty()) {
+			Node cur = q.poll();
+
+			for (Node x : adj[cur.vertex]) {
+				if (dis[cur.vertex] + x.weight < dis[x.vertex]) {
+					q.add(x);
+					dis[x.vertex] = dis[cur.vertex] + x.weight;
+				}
+			}
+		}
+		
+		for (int i = 1; i <= n; i ++) {
+			System.out.println(dis[i] != Integer.MAX_VALUE ? dis[i] : -1);
+		}
+	}
+}
