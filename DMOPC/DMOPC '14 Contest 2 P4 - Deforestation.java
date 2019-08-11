7ea9641254b1d04eb3cd161bedb79ebab01210d1
Austin-X <32133198+Austin-X@users.noreply.github.com>
2019-08-11 16:24:23 -0400

    Update DMOPC '14 Contest 2 P4 - Deforestation.java

--- DMOPC/DMOPC '14 Contest 2 P4 - Deforestation.java
+++ DMOPC/DMOPC '14 Contest 2 P4 - Deforestation.java
@@ -7,25 +7,16 @@ public class Deforestation {
 		StringTokenizer st;
 		
 		int n = Integer.parseInt(br.readLine());
-		int[] sum = new int[n + 1];
-		int[] arr = new int[n + 1];
-		
-		arr[0] = 0; sum[0] = 0;
-		for (int i = 1; i <= n; i ++) {
-			arr[i] = Integer.parseInt(br.readLine());
-			if (i == 1) sum[i] = arr[i];
-			else {
-				sum[i] = sum[i - 1] + arr[i];
-			}
-		}
+		int[] psa = new int[n + 1];
+		psa[1] = Integer.parseInt(br.readLine());
+		for (int i = 2; i <= n; i ++) psa[i] = psa[i - 1] + Integer.parseInt(br.readLine());
 		
 		int q = Integer.parseInt(br.readLine());
 		for (int i = 0; i < q; i ++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			
-			System.out.println(a == b ? arr[a + 1] : sum[b + 1] - sum[a]);
+			System.out.println(psa[b + 1] - psa[a]);
 		}
 	}
 }
