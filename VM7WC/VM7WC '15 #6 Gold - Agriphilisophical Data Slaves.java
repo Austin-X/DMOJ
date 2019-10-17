1767f6f8f7f6e3a74b759514956abbe48ad5a663
Austin-X <32133198+Austin-X@users.noreply.github.com>
2019-10-16 20:43:09 -0400

    Create VM7WC '15 #6 Gold - Agriphilisophical Data Slaves.java

---
+++ VM7WC/VM7WC '15 #6 Gold - Agriphilisophical Data Slaves.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class AgriphilisophicalDataSlaves {
+	static int[] cn, characters;
+	static ArrayList<Integer>[] adj;
+	static boolean[] visited;
+	static Stack<Integer> arr = new Stack<Integer>();
+	
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		cn = new int[n + 1];
+		visited = new boolean[n + 1];
+
+		adj = new ArrayList[n + 1];
+		for (int i = 1; i <= n; i ++) adj[i] = new ArrayList<Integer>();
+		
+		for (int i = 0; i < n - 1; i ++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			adj[x].add(y);
+		}
+		
+		characters = new int[n + 1];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i ++) {
+			characters[i] = Integer.parseInt(st.nextToken());
+			cn[i] = characters[i];
+		}
+		
+		for (int i = 1; i <= n; i ++) {
+			if (!visited[i]) recurse(i, 0); 
+		}
+		
+		int max = -Integer.MAX_VALUE;
+		for (int i = 1; i <= n; i ++) max = Math.max(max, cn[i]);
+		System.out.println(max);
+	}
+	
+	static void recurse(int x, int curValue) {
+		visited[x] = true;
+		arr.push(x);
+		for (int u : adj[x]) {
+			if (!visited[u]) {
+				recurse(u, curValue);
+				for (int i : arr) cn[i] += characters[u];
+			}
+		}
+		arr.pop();
+	}
+}
