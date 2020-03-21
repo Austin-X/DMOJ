5b0f54e8801edb89fde3b1ebe2c82c86bd073608
Austin-X <32133198+Austin-X@users.noreply.github.com>
2020-03-21 15:51:40 -0400

    Create USACO 2019 December Bronze P3 - Livestock Lineup.java

---
+++ Olympiads/USACO 2019 December Bronze P3 - Livestock Lineup.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class LivestockLineup {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+        
+		String[] temp = {"Beatrice", "Belinda", "Bella", "Bessie", "Betsy", "Blue", "Buttercup", "Sue"};
+		ArrayList<String> order = new ArrayList<String>();
+		for (String x : temp) order.add(x);
+		
+		@SuppressWarnings("unchecked")
+		ArrayList<String>[] adj = new ArrayList[8];
+		for (int i = 0; i < 8; i ++) adj[i] = new ArrayList<String>();
+		
+		for (int t = 0; t < n; t ++) {
+			String[] parts = br.readLine().split(" ");
+			String cow1 = parts[0], cow2 = parts[5];
+			adj[order.indexOf(cow1)].add(cow2);
+			adj[order.indexOf(cow2)].add(cow1);
+		}	
+		boolean[] vis = new boolean[8];
+
+		while (true) {
+			String str = "z";
+			int idx = -1;
+			
+			for (int i = 7; i >= 0; i--) {
+				if (!vis[i] && adj[i].size() < 2) {
+					str = temp[i]; idx = i;
+				}
+			}
+			if (idx == -1) break;
+			
+			System.out.println(str);
+			vis[idx] = true;
+			for (int i = 0; i < 8; i ++) {
+				if (adj[i].contains(str)) adj[i].remove(adj[i].indexOf(str));
+			}
+			
+			while (!adj[idx].isEmpty()) {
+				for (String x : adj[idx]) {
+					System.out.println(x); vis[order.indexOf(x)] = true; idx = order.indexOf(x); str = x;
+				}
+				
+				for (int i = 0; i < 8; i ++) {
+					if (adj[i].contains(str)) adj[i].remove(adj[i].indexOf(str));
+				}
+			}
+		}
+	}
+}
