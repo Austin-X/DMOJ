81e0a5a38fcef493b69aa2d0ed0a8efd92e1dd0d
Austin-X <32133198+Austin-X@users.noreply.github.com>
2020-03-24 11:02:08 -0400

    Create Summing A Sequence.java

---
+++ Uncategorized/Summing A Sequence.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class SummingASequence {
+	static int[] nums;
+	static long[] dp;
+	static int n;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		n = Integer.parseInt(br.readLine());
+		nums = new int[n];
+		dp = new long[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i ++) nums[i] = Integer.parseInt(st.nextToken());
+		
+
+		if (nums[0] > 0) dp[0] = nums[0];	
+		if (n > 1) dp[1] = Math.max(nums[1], dp[0]); 
+		for (int i = 2; i < n; i ++) {
+			dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+		}
+		
+		System.out.println(Arrays.toString(dp));
+		System.out.println(dp[n - 1]);
+	}
+}
