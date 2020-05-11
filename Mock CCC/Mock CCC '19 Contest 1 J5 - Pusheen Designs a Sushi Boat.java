641087895d9e9973b50c7624625fa5a2996f289e
Austin-X <32133198+Austin-X@users.noreply.github.com>
2020-05-11 11:12:12 -0400

    Update Mock CCC '19 Contest 1 J5 - Pusheen Designs a Sushi Boat.java

--- Mock CCC/Mock CCC '19 Contest 1 J5 - Pusheen Designs a Sushi Boat.java
+++ Mock CCC/Mock CCC '19 Contest 1 J5 - Pusheen Designs a Sushi Boat.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class PusheenDesignsASushiBoat {
-	static int MOD = 998244353;
+	static final int MOD = 998244353;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
