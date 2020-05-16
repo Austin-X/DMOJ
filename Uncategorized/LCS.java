e0076e54e39b96fe45cd81789df74311860bfaf7
Austin-X <32133198+Austin-X@users.noreply.github.com>
2020-05-16 15:45:41 -0400

    Update LCS.java

--- Uncategorized/LCS.java
+++ Uncategorized/LCS.java
@@ -12,10 +12,7 @@ public class LCS {
 		for (int i = 1; i <= n; i ++) {
 			for (int j = 1; j <= m; j ++) {
 				if (a.charAt(i - 1) == b.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1] + 1;
-				else {
-					if (dp[i - 1][j] >= dp[i][j - 1]) dp[i][j] = dp[i - 1][j]; 
-					else dp[i][j] = dp[i][j - 1]; 
-				}
+				else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
 			}
 		}
 		
