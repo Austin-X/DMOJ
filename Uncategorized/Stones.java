bb10a359342fbb2b0d56ba3b9c18c0fb80660af8
Austin-X <32133198+Austin-X@users.noreply.github.com>
2020-06-06 00:15:01 -0400

    Update Stones.java

--- Uncategorized/Stones.java
+++ Uncategorized/Stones.java
@@ -8,22 +8,20 @@ public class Stones {
 		
 		int n = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
 		int[] arr = new int[n];
-		int[] dp = new int[k + 1];
+		boolean[] dp = new boolean[k + 1];
 		
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i ++) arr[i] = Integer.parseInt(st.nextToken());
 		
-		dp[0] = 1;
+		dp[0] = false;
 		for (int i = 1; i <= k; i ++) {
-			boolean flag = false;
 			for (int j = 0; j < n; j ++) {
-				if (i - arr[j] >= 0 && dp[i - arr[j]] == 1) {
-					flag = true; break;
+				if (i - arr[j] >= 0 && !dp[i - arr[j]]) {
+					dp[i] = true; break;
 				}
 			}
-			dp[i] = flag ? 2 : 1;
 		}
 		
-		System.out.println(dp[k] == 2 ? "First" : "Second");
+		System.out.println(dp[k] ? "First" : "Second");
 	}
 }
