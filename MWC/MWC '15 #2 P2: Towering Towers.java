e0cbdad167e4abbf317280f8ab691acd87858453
Austin-X <32133198+Austin-X@users.noreply.github.com>
2020-07-20 14:27:55 -0400

    Update MWC '15 #2 P2: Towering Towers.java

--- MWC/MWC '15 #2 P2: Towering Towers.java
+++ MWC/MWC '15 #2 P2: Towering Towers.java
@@ -2,29 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class ToweringTowers {
+	static class Pair {
+		int height, idx;
+		Pair (int height, int idx) { this.height = height; this.idx = idx; }
+	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 		int n = Integer.parseInt(br.readLine());
 		int[] towers = new int[n];
-		int[] see = new int[n];
 		
 		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i ++) towers[i] = Integer.parseInt(st.nextToken());
+
+		Stack<Pair> s = new Stack<Pair>();
 		for (int i = 0; i < n; i ++) {
-			towers[i] = Integer.parseInt(st.nextToken());
-		}
-		see[0] = 0;
-		for (int i = 1; i < n; i ++) {
-			int cn = 1;
-			for (int j = i - 1; j > 0; j --) {
-				if (towers[i] >= towers[j]) {
-					cn ++;
-				} else break;
+			if (i == 0) { System.out.print(0 + " "); s.push(new Pair(towers[i], 0)); }
+			else {
+				while (!s.isEmpty() && towers[i] >= s.peek().height) s.pop();
+				if (s.isEmpty()) System.out.print(i + " ");
+				else System.out.print((i - s.peek().idx) + " ");
+				s.push(new Pair(towers[i], i));
 			}
-			see[i] = cn;
 		}
-		
-		for (int x : see) System.out.print(x + " ");
 	}
 }
