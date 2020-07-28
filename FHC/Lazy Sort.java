48973cd6e69df99c65e3c11b8598aceac8044331
Austin-X <32133198+Austin-X@users.noreply.github.com>
2020-07-27 22:59:39 -0400

    Update Lazy Sort.java

--- FHC/Lazy Sort.java
+++ FHC/Lazy Sort.java
@@ -14,36 +14,28 @@ public class LazySort {
 			for (int i = 0; i < n; i ++) original.addLast(readInt());
 			Deque<Integer> q = new ArrayDeque<Integer>();
 			
-			recurse(n, 0, n - 1, original, q);
+			recurse(n, original, q);
 			System.out.print("Case #" + t + ": ");
 			System.out.println(flag ? "yes" : "no");
 		}
 	}
-	static void recurse(int n, int low, int high, Deque<Integer> original, Deque<Integer> q) {
+	static void recurse(int n, Deque<Integer> original, Deque<Integer> q) {
 		if (q.size() == n || n == 1) { flag = true; return; }
 		
 		int x;
 		if (q.isEmpty()) {
 			x = original.pollFirst();
 			q.addFirst(x);
-			recurse(n, 1, high, original, q);
+			recurse(n, original, q);
 			q.pollFirst();
 			original.addFirst(x);
 			q.addFirst(original.pollLast());
-			recurse(n, 0, high - 1, original, q);
+			recurse(n, original, q);
 		} else {
-			if (low + 1 < n && original.peekFirst() + 1 == q.peekFirst()) {
-				x = original.pollFirst(); q.addFirst(x); recurse(n, low + 1, high, original, q); original.addFirst(x); q.pollFirst();
-			}
-			else if (high - 1 >= 0 && original.peekLast() + 1 == q.peekFirst()) {
-				x = original.pollLast(); q.addFirst(x); recurse(n, low, high - 1, original, q); original.addLast(x); q.pollFirst();
-			}	
-			else if (low + 1 < n && original.peekFirst() == q.peekLast() + 1) {
-				x = original.pollFirst(); q.addLast(x); recurse(n, low + 1, high, original, q); original.addFirst(x); q.pollLast();
-			}
-			else if (high - 1 >= 0 && original.peekLast() == q.peekLast() + 1) {
-				x = original.pollLast(); q.addLast(x); recurse(n, low, high - 1, original, q); original.addLast(x); q.pollLast();
-			}
+			if (original.peekFirst() + 1 == q.peekFirst()) { x = original.pollFirst(); q.addFirst(x); recurse(n, original, q); original.addFirst(x); q.pollFirst(); }
+			else if (original.peekLast() + 1 == q.peekFirst()) { x = original.pollLast(); q.addFirst(x); recurse(n, original, q); original.addLast(x); q.pollFirst(); }	
+			else if (original.peekFirst() == q.peekLast() + 1) { x = original.pollFirst(); q.addLast(x); recurse(n, original, q); original.addFirst(x); q.pollLast(); }
+			else if (original.peekLast() == q.peekLast() + 1) { x = original.pollLast(); q.addLast(x); recurse(n, original, q); original.addLast(x); q.pollLast(); }
 		}
 	}
 	static String next() throws IOException {
