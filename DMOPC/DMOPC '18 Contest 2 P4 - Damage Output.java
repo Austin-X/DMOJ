6b6d1b3b573c0c240c6bc15fe6cac634603ad197
Austin-X <32133198+Austin-X@users.noreply.github.com>
2020-07-28 11:34:29 -0400

    Update DMOPC '18 Contest 2 P4 - Damage Output.java

--- DMOPC/DMOPC '18 Contest 2 P4 - Damage Output.java
+++ DMOPC/DMOPC '18 Contest 2 P4 - Damage Output.java
@@ -2,30 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class DamageOutput {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
 	public static void main (String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		double m = Double.parseDouble(st.nextToken());
-		long[] damage = new long[n];
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i ++) damage[i] = Integer.parseInt(st.nextToken());
-		
-		int low = 0, high = 0, ans = Integer.MAX_VALUE;
-		long sum = damage[0];
-		while (low <= high) {
-			if (sum < m) {
-				high ++;
-				if (high == n) break;
-				sum += damage[high];
-			} else {
-				ans = Math.min(ans, high - low + 1);
-				sum -= damage[low];
-				low ++;
+		int n = readInt(); long m = readLong();
+		Deque<Integer> q = new ArrayDeque<Integer>();
+		long sum = 0, ans = Integer.MAX_VALUE;
+		for (int i = 0; i < n; i ++) {
+			int x = readInt();	
+			sum += x;
+			q.addLast(x);
+			
+			while (true) {
+				if (sum - q.peekFirst() < m) break;
+				else sum -= q.pollFirst();
 			}
+			if (sum >= m) ans = Math.min(ans, q.size());
+			if (ans == 1) break;
 		}
-		System.out.println(ans != Integer.MAX_VALUE ? ans : -1);
+
+		System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
+	}
+	
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) 
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine() throws IOException {
+		return br.readLine().trim();
 	}
 }
