c8ed734c94b4e75b400675ba08b16ec9cfc3bb83
Austin-X <32133198+Austin-X@users.noreply.github.com>
2020-08-03 15:14:54 -0400

    Update COCI '07 Contest 4 #3 Lektira.java

--- COCI/COCI '07 Contest 4 #3 Lektira.java
+++ COCI/COCI '07 Contest 4 #3 Lektira.java
@@ -1,22 +1,44 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Lektira {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String word = br.readLine();
-		String bestWord = "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
-		
-		// Checking all the possible cases
-		for (int i = 1; i < word.length() - 2; i ++) {
-			for (int j = i + 1; j < word.length() - 1; j ++) {
-				String part1 = new StringBuilder(word.substring(0, i)).reverse().toString();
-				String part2 = new StringBuilder(word.substring(i, j + 1)).reverse().toString();
-				String part3 = new StringBuilder(word.substring(j + 1)).reverse().toString();
-				if ((part1 + part2 + part3).compareTo(bestWord) < 0) 
-					bestWord = part1 + part2 + part3;
+		String word = readLine();
+		String best = "{";
+		for (int i = 1; i < word.length() - 1; i ++) {
+			for (int l = i + 1; l < word.length(); l ++) {
+				String part1 = word.substring(0, i), part2 = word.substring(i, l), part3 = word.substring(l);
+				String temp1 = "", temp2 = "", temp3 = "";
+				for (int j = part1.length() - 1; j >= 0; j --) temp1 += part1.charAt(j);
+				for (int j = part2.length() - 1; j >= 0; j --) temp2 += part2.charAt(j);
+				for (int j = part3.length() - 1; j >= 0; j --) temp3 += part3.charAt(j);
+				String str = temp1 + temp2 + temp3;
+				if (str.compareTo(best) < 0) best = str;
 			}
 		}
-		System.out.println(bestWord);
+		System.out.println(best);
+	}
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) 
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine() throws IOException {
+		return br.readLine().trim();
 	}
 }
