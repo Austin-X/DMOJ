659d3ceb17a7828b5c2190695076cd5187cecc26
Austin-X <32133198+Austin-X@users.noreply.github.com>
2020-08-03 15:14:01 -0400

    Update COCI '07 Contest 2 #3 Prva.java

--- COCI/COCI '07 Contest 2 #3 Prva.java
+++ COCI/COCI '07 Contest 2 #3 Prva.java
@@ -1,49 +1,56 @@
-import java.util.*;
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Prva {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String smallestWord = "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
-		String[] temp = br.readLine().split(" ");
-		int rows = Integer.parseInt(temp[0]);
-		int cols = Integer.parseInt(temp[1]);
-		char[][] chars = new char[rows][cols];
-		
-		for (int i = 0; i < rows; i ++) {
-			chars[i] = br.readLine().toCharArray();
-		}
-		
-		String word;
-		for (int i = 0; i < rows; i ++) {
-			word = "";
-			for (int j = 0; j < cols; j ++) {
-				if (chars[i][j] == '#') {
-					if (word.compareTo(smallestWord) < 0 && word.length() >= 2) smallestWord = word;
-					word = "";
+		int r = readInt(), c = readInt();
+		char[][] grid = new char[r][c];
+		for (int i = 0; i < r; i ++) grid[i] = readLine().toCharArray();
+		String ans = "{";
+		for (int i = 0; i < r; i ++) {
+			for (int j = 0; j < c; j ++) {
+				String temp = "";
+				if (j == 0 || grid[i][j - 1] == '#') {
+					for (int k = j; k < c; k ++) {
+						if (grid[i][k] == '#') break;
+						else temp += grid[i][k];
+					}
 				}
-				else {
-					word += chars[i][j];
+				if (temp.length() >= 2 && temp.compareTo(ans) < 0) ans = temp;
+				temp = "";
+				
+				if (i == 0 || grid[i - 1][j] == '#') {
+					for (int k = i; k < r; k ++) {
+						if (grid[k][j] == '#') break;
+						else temp += grid[k][j];
+					}
 				}
+				if (temp.length() >= 2 && temp.compareTo(ans) < 0) ans = temp;
 			}
-			if (word.compareTo(smallestWord) < 0  && word.length() >= 2) smallestWord = word;
 		}
-		
-		for (int i = 0; i < cols; i ++) {
-			word = "";
-			for (int j = 0; j < rows; j ++) {
-				if (chars[j][i] == '#') {
-					if (word.compareTo(smallestWord) < 0 && word.length() >= 2) smallestWord = word;
-					word = "";
-				}
-				else {
-					word += chars[j][i];
-				}
-			}
-			if (word.compareTo(smallestWord) < 0  && word.length() >= 2) smallestWord = word;
-		}
-		
-		System.out.println(smallestWord);
+		System.out.println(ans);
+	}
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) 
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine() throws IOException {
+		return br.readLine().trim();
 	}
 }
