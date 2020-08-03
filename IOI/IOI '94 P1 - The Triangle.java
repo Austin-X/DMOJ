c66707d9000469accf7185b2ab3d150e973586bd
Austin-X <32133198+Austin-X@users.noreply.github.com>
2020-08-03 17:31:11 -0400

    Update IOI '94 P1 - The Triangle.java

--- IOI/IOI '94 P1 - The Triangle.java
+++ IOI/IOI '94 P1 - The Triangle.java
@@ -2,45 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class TheTriangle {
-	static int[][] dp;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		int n = readInt();
 		
-		int n = Integer.parseInt(br.readLine());
-		dp = new int[n][n];
-		for (int[] x : dp) Arrays.fill(x, -1);
-		
-		int cn = 1;
+		int cn = 1; int[][] dp = new int[n][n];
 		for (int i = 0; i < n; i ++) {
-			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < cn; j ++) {
-				dp[i][j] = Integer.parseInt(st.nextToken());
+				if (cn == 1) dp[i][j] = readInt();
+				else if (j == 0) dp[i][j] = dp[i - 1][j] + readInt();
+				else if (j == cn - 1) dp[i][j] = dp[i - 1][j - 1] + readInt();
+				else dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + readInt();
 			}
 			cn ++;
 		}
-		
-		System.out.println(solve(n));
+
+		int max = 0;
+		for (int i = 0; i < n; i ++) max = Math.max(max, dp[n - 1][i]);
+		System.out.println(max);
 	}
-	
-	static int solve(int n) {
-		int max = 0, cn = 1;
-		
-		for (int i = 1; i < n; i ++) {
-			for (int j = 0; j < n; j ++) {
-				if (j == 0) dp[i][j] += dp[i - 1][j];
-				else if (j == cn) dp[i][j] += dp[i - 1][j - 1];
-				else {
-					dp[i][j] += Math.max(dp[i - 1][j - 1], dp[i - 1][j]);
-				}
-			}
-			cn ++;
-		}
-		
-		for (int i = 0; i < n; i ++) {
-			max = Math.max(dp[n - 1][i], max);
-		}
-		
-		return max;
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) 
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine() throws IOException {
+		return br.readLine().trim();
 	}
 }
