8cca24d1dad10e190d6bb796cd2e9fac4890ac72
Austin-X <32133198+Austin-X@users.noreply.github.com>
2020-08-14 16:48:53 -0400

    Update MNYC '17: Hurontario.java

--- Seasonal/MNYC '17: Hurontario.java
+++ Seasonal/MNYC '17: Hurontario.java
@@ -6,18 +6,17 @@ public class Hurontario {
 	static StringTokenizer st;
 	public static void main(String[] args) throws IOException {
 		String a = next(), b = next();
-		int base = 131; long[] hsh1 = new long[a.length()], hsh2 = new long[b.length()];
-		long[] pow = new long[a.length()];
+		int base = 131, idx = 0;
+		long hsh1 = 0, hsh2 = 0; long[] pow = new long[a.length()];
 		for (int i = a.length() - 1; i >= 0; i --) pow[i] = i == a.length() - 1 ? 1 : pow[i + 1] * base;
-		for (int i = a.length() - 1; i >= 0; i --) hsh1[i] = i == a.length() - 1 ? a.charAt(i) : a.charAt(i) * pow[i] + hsh1[i + 1];
-		for (int i = 0; i < b.length(); i ++) hsh2[i] = i == 0 ? b.charAt(i) : hsh2[i - 1] * base + b.charAt(i); 
-		for (int i = 0; i < a.length(); i ++) {
-			if (a.length() - i - 1 < b.length() && hsh1[i] == hsh2[a.length() - i - 1]) {
-				System.out.print(a.substring(0, i)); System.out.println(b);
-				System.exit(0);
-			}
+	
+		int best = a.length();
+		for (int i = a.length() - 1; i >= 0; i --) {
+			if (idx == b.length()) break;
+			hsh1 += a.charAt(i) * pow[i]; hsh2 = hsh2 * base + b.charAt(idx); idx ++;
+			if (hsh1 == hsh2) best = i;
 		}
-		System.out.print(a); System.out.println(b);
+		System.out.print(a.substring(0, best)); System.out.println(b);
 	}
 
 	static String next() throws IOException {
