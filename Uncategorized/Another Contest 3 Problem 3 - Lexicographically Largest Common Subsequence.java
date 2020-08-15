9249e047ae7e041e99e4c7b56d5635df731e89e9
Austin-X <32133198+Austin-X@users.noreply.github.com>
2020-08-15 14:35:42 -0400

    Update Another Contest 3 Problem 3 - Lexicographically Largest Common Subsequence.java

--- Uncategorized/Another Contest 3 Problem 3 - Lexicographically Largest Common Subsequence.java
+++ Uncategorized/Another Contest 3 Problem 3 - Lexicographically Largest Common Subsequence.java
@@ -7,44 +7,27 @@ public class LexicographicallyLargestCommonSubsequence {
 	public static void main(String[] args) throws IOException {
 		int n = readInt();
 		String[] arr = new String[n];
-		int[] cnAns = new int[26], cnTemp = new int[26];
-		for (int i = 0; i < n; i ++) {
-			arr[i] = readLine();
-			if (i == 0) {
-				for (int j = 0; j < arr[i].length(); j ++) cnAns[arr[i].charAt(j) - 'a'] ++;
-			} else {
-				for (int j = 0; j < arr[i].length(); j ++) cnTemp[arr[i].charAt(j) - 'a'] ++;
-				for (int j = 0; j < 26; j ++) cnAns[j] = Math.min(cnAns[j], cnTemp[j]);
-			}
-			Arrays.fill(cnTemp, 0);
-		}
-
-		boolean pass = false; int[] curIdx = new int[n];
+		for (int i = 0; i < n; i ++) arr[i] = readLine();
+		int[] curIdx = new int[n]; boolean flag = false;
 		for (int i = 25; i >= 0; i --) {
-			if (cnAns[i] != 0) {
-				int max = cnAns[i];
+			boolean found = true;
+			while (found) {
+				int[] temp = new int[n];
 				for (int j = 0; j < n; j ++) {
-					int temp = 0;
-					for (int k = 0; k < arr[j].length(); k ++) if (arr[j].charAt(k) == (char)(i + 'a') && k >= curIdx[j]) temp ++; 
-					max = Math.min(max, temp);
-				}
-				
-				if (max > 0) {
-					pass = true;
-					for (int j = 0; j < n; j ++) {
-						int temp = 0;
-						for (int k = 0; k < arr[j].length(); k ++) {
-							if (k >= curIdx[j] && arr[j].charAt(k) == (char)(i + 'a')) temp ++;
-							if (temp == max) { curIdx[j] = k; break; }
-						}
+					found = false;
+					for (int k = curIdx[j]; k < arr[j].length(); k ++) {
+						if (arr[j].charAt(k) == (char)(i + 'a')) { temp[j] = k; found = true; break; }
 					}
-					for (int j = 0; j < max; j ++) System.out.print((char)(i + 'a'));
+					if (!found) break;
+				}
+				if (found) {
+					for (int j = 0; j < n; j ++) curIdx[j] = temp[j] + 1;
+					System.out.print((char)(i + 'a'));  
+					flag = true;
 				}
 			}
 		}
-		
-		if (!pass) System.out.print(-1);
-		System.out.println();
+		System.out.println(flag ? "" : -1);
 	}
 
 	static String next() throws IOException {
