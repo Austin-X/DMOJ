6c3639a44478dffe2adbe9897971876bf975b621
Austin-X <32133198+Austin-X@users.noreply.github.com>
2020-08-19 15:51:00 -0400

    Update VM7WC '15 #2 Silver - 4 Nations, 1 Secret.java

--- VM7WC/VM7WC '15 #2 Silver - 4 Nations, 1 Secret.java
+++ VM7WC/VM7WC '15 #2 Silver - 4 Nations, 1 Secret.java
@@ -5,7 +5,7 @@ public class FourNations1Secret {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	public static void main(String[] args) throws IOException {
-		int n = readInt(), idxAns = 0, length = 1; String s = readLine();
+		int n = readInt(), idxAns = 1, length = 1; String s = readLine();
 		char[] str = s.toCharArray();
 		long[] prefixHsh = new long[n + 1], suffixHsh = new long[n + 2], pow = new long[n + 1]; int seed = 131; pow[0] = 1;
 		for (int i = 1; i <= n; i ++) {
@@ -13,23 +13,32 @@ public class FourNations1Secret {
 			pow[i] = pow[i - 1] * seed;
 		}
 		for (int i = n; i >= 1; i --) suffixHsh[i] = suffixHsh[i + 1] * seed + str[i - 1];
+
 		for (int i = 1; i <= n; i ++) {
-			int diff = 2, best = 1;
-			while (i - diff >= 0 && i + diff <= n + 1) {
-				if (prefixHsh[i] - prefixHsh[i - diff] * pow[diff] == suffixHsh[i] - suffixHsh[i + diff] * pow[diff]) { best += 2; diff ++; }
-				else break;
+			int low = i, high = n + 1;
+			while (low != high) {
+				int mid = (low + high) / 2;
+				if (i - (mid - i) < 0) high = mid;
+				else if (prefixHsh[i] - prefixHsh[i - (mid - i)] * pow[mid - i] == suffixHsh[i] - suffixHsh[mid] * pow[mid - i]) low = mid + 1;
+				else high = mid;
 			}
-			if (best > length) { length = best; idxAns = i - best / 2 - 1; }
+			if (i - (low - i) >= 0 && prefixHsh[i] - prefixHsh[i - (low - i)] * pow[low - i] == suffixHsh[i] - suffixHsh[low] * pow[low - i]) {
+				if ((low - i - 1) * 2 + 1 > length) { length = (low - i - 1) * 2 + 1; idxAns = low - 1; }
+			}
+			else if ((low - i - 2) * 2 + 1 > length) { length = (low - i - 2) * 2 + 1; idxAns = low - 2; }
+			
 			if (i != n && str[i - 1] == str[i]) {
-				int low = i, high = i + 1; diff = 1; best = 0;
-				while (low - diff >= 0 && high + diff <= n + 1) {
-					if (prefixHsh[low] - prefixHsh[low - diff] * pow[diff] == suffixHsh[high] - suffixHsh[high + diff] * pow[diff]) { best += 2; diff ++; }
-					else break;
+				low = i; high = n + 1;
+				while (low != high) {
+					int mid = (low + high) / 2;
+					if (i - (mid - i) < 0 || mid + 1 > n + 1) high = mid;
+					else if (prefixHsh[i] - prefixHsh[i - (mid - i)] * pow[mid - i] == suffixHsh[i + 1] - suffixHsh[mid + 1] * pow[mid - i]) low = mid + 1;
+					else high = mid;
 				}
-				if (best > length) { length = best; idxAns = i - best / 2; }
+				if ((low - 1 - i) * 2 > length) { length = (low - 1 - i) * 2 ; idxAns = low - 1; }
 			}
 		}
-		System.out.println(s.substring(idxAns, idxAns + length));
+		System.out.println(s.substring(idxAns - length, idxAns));
 		System.out.println(length);
 	}
 
