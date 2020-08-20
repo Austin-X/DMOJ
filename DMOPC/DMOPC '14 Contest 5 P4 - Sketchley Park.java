a901545d2a2fc7ef19ffc810ed0e63104e2ab79a
Austin-X <32133198+Austin-X@users.noreply.github.com>
2020-08-20 15:12:51 -0400

    Update DMOPC '14 Contest 5 P4 - Sketchley Park.java

--- DMOPC/DMOPC '14 Contest 5 P4 - Sketchley Park.java
+++ DMOPC/DMOPC '14 Contest 5 P4 - Sketchley Park.java
@@ -2,41 +2,57 @@ import java.io.*;
 import java.util.*;
 
 public class SketchleyPark {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[] str = readLine().toCharArray(); int len = str.length;
+		if (len < 9) { System.out.println("KeyNotFoundError"); System.exit(0); }
 		
-		char[] message = br.readLine().toCharArray();
-		int len = message.length;
-		
-		int n = Integer.parseInt(br.readLine());
-		
-		HashSet<String> possible = new HashSet<String>();
-		
-		String str;
+		HashSet<String> possible = new HashSet<String>(); 
 		for (int i = 0; i <= len - 9; i ++) {
-			if (message[i] == message[i + 4] && message[i + 1] == message[i + 8]) {
-				str = "";
-				str += message[i]; str += message[i + 1]; str += message[i + 2]; str += message[i + 3];
-				str += message[i + 5]; str += message[i + 6]; str += message[i + 7];
-				possible.add(str);
+			if (str[i] == str[i + 4] && str[i + 1] ==  str[i + 8] && str[i + 2] != str[i + 3] && str[i + 3] != str[i + 5] && str[i + 5] != str[i + 6] && str[i + 6] != str[i + 7]) {
+				String s = ""; for (int j = i; j < i + 9; j ++) s += str[j];
+				possible.add(s);
 			}
 		}
-		
-		if (!possible.isEmpty()) {
-			for (int t = 0; t < n; t ++) {
-				char[] key = br.readLine().toCharArray();
-				
-				str = "";
-				str += key[7]; str += key[0]; str += key[8]; str += key[11]; str +=key[24]; str += key[3]; str += key[17];
-				
-				if (possible.contains(str)) {
-					HashMap<Character, Character> map = new HashMap<Character, Character>();
-					for (int k = 0; k < 26; k ++) map.put(key[k], (char)(k + 65));
-					for (int k = 0; k < len; k ++) System.out.print(map.get(message[k]));
-					System.exit(0);
-				}
+
+		char[] keys = new char[26];
+		int n = readInt();
+		for (int i = 0; i < n; i ++) {
+			char[] temp = readLine().toCharArray();
+			HashMap<Character, Character> map = new HashMap<Character, Character>();
+			for (int j = 0; j < 26; j ++) {
+				keys[j] = temp[j]; map.put(temp[j], (char)(j + 'A'));
+			}
+			String decryp = ""; decryp += keys[7]; decryp += keys[0]; 
+			decryp += keys[8]; decryp += keys[11]; decryp += keys[7]; 
+			decryp += keys[24]; decryp += keys[3]; decryp += keys[17]; decryp += keys[0];
+			if (possible.contains(decryp)) {
+				for (int j = 0; j < len; j ++) System.out.print(map.get(str[j]));
+				System.exit(0);
 			}
 		}
 		System.out.println("KeyNotFoundError");
+	}
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) 
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine() throws IOException {
+		return br.readLine().trim();
 	}
 }
