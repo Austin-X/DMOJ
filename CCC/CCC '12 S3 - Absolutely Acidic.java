3d153b7f90b26280aadd105bdb80bd2e59dce1ff
Austin-X <32133198+Austin-X@users.noreply.github.com>
2020-08-27 15:50:32 -0400

    Update CCC '12 S3 - Absolutely Acidic.java

--- CCC/CCC '12 S3 - Absolutely Acidic.java
+++ CCC/CCC '12 S3 - Absolutely Acidic.java
@@ -2,64 +2,65 @@ import java.io.*;
 import java.util.*;
 
 public class AbsolutelyAcidic {
-	static class pair implements Comparable<pair>{
-		int number, count;
-		
-		pair(int number, int count) {
-			this.number = number;
-			this.count = count;
-		}
-		
-		public int compareTo(pair p) {
-			return -Integer.compare(count, p.count);
+	static class Pair implements Comparable<Pair>{
+		int idx, val;
+		Pair (int idx, int val) { this.idx = idx; this.val = val; }
+
+		public int compareTo(Pair p) { 
+			return Integer.compare(val, p.val);
 		}
 	}
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
 	public static void main (String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = readInt();
+		ArrayList<Pair> arr = new ArrayList<Pair>();
+		int[] freq = new int[1001];
+		for (int i = 0; i < n; i ++) freq[readInt()] ++;
 		
-		int n = Integer.parseInt(br.readLine());
-		
-		int[] readings = new int[n];
-		int[] cn = new int[1001];
-		for (int i = 0; i < n; i ++) {
-			readings[i] = Integer.parseInt(br.readLine());
-			cn[readings[i]] ++;
-		}
-		
-		ArrayList<pair> nums = new ArrayList<pair>();
 		for (int i = 1; i <= 1000; i ++) {
-			if (cn[i] != 0) nums.add(new pair(i, cn[i]));
+			if (freq[i] != 0) arr.add(new Pair(i, freq[i]));
 		}
-		Collections.sort(nums);
+		Collections.sort(arr);
 		
-		ArrayList<Integer> freq1 = new ArrayList<Integer>();
-		freq1.add(nums.get(0).number); 
-		ArrayList<Integer> freq2 = new ArrayList<Integer>();
-		
-		int check = 0;
-		for (int i = 1; i < nums.size(); i ++) {
-			if (nums.get(i).count == nums.get(i - 1).count) { 
-				if (check == 0) freq1.add(nums.get(i).number);
-				else freq2.add(nums.get(i).number);
-			}
-			else {
-				if (check == 1) break;
-				freq2.add(nums.get(i).number);
-				check ++;
-			}
+		int max, secondMax;
+		if (arr.get(arr.size() - 1).val == arr.get(arr.size() - 2).val) { max = arr.get(arr.size() - 1).val; secondMax = max; }
+		else { max = arr.get(arr.size() - 1).val; secondMax = arr.get(arr.size() - 2).val; }
+	
+		ArrayList<Integer> m = new ArrayList<Integer>();
+		for (int i = 0; i < arr.size(); i ++) {
+			if (arr.get(i).val == max) m.add(arr.get(i).idx);
 		}
-		
-		Collections.sort(freq1);
-		Collections.sort(freq2);
-
-		if (freq1.size() >= 2) System.out.println(Math.abs(freq1.get(freq1.size() - 1) - freq1.get(0)));
+		Collections.sort(m);
+		if (max == secondMax) System.out.println(m.get(m.size() - 1) - m.get(0));
 		else {
-			int max = 0;
-			for (int i = 0; i < freq2.size(); i ++) {
-				if (Math.abs(freq1.get(0) - freq2.get(i)) < max) break;
-				else max = Math.abs(freq1.get(0) - freq2.get(i));
-			}
-			System.out.println(max);
+			ArrayList<Integer> m2 = new ArrayList<Integer>();
+			for (int i = 0; i < arr.size(); i ++) if (arr.get(i).val == secondMax) m2.add(arr.get(i).idx);
+			Collections.sort(m2);
+			int ans = 0;
+			for (int i = 0; i < m.size(); i ++) ans = Math.max(ans, Math.max(Math.abs(m.get(i) - m2.get(0)), Math.abs(m.get(i) - m2.get(m2.size() - 1))));
+			System.out.println(ans);
 		}
+	} 
+	
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) 
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine() throws IOException {
+		return br.readLine().trim();
 	}
 }
