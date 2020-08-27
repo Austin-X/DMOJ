bd4a115bbeb03db2017ec80ae4bfa717a2005a84
Austin-X <32133198+Austin-X@users.noreply.github.com>
2020-08-27 16:26:57 -0400

    Update CCC '12 S3 - Absolutely Acidic.java

--- CCC/CCC '12 S3 - Absolutely Acidic.java
+++ CCC/CCC '12 S3 - Absolutely Acidic.java
@@ -2,43 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class AbsolutelyAcidic {
-	static class Pair implements Comparable<Pair>{
-		int idx, val;
-		Pair (int idx, int val) { this.idx = idx; this.val = val; }
-
-		public int compareTo(Pair p) { 
-			return Integer.compare(val, p.val);
-		}
-	}
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	public static void main (String[] args) throws IOException {
 		int n = readInt();
-		ArrayList<Pair> arr = new ArrayList<Pair>();
 		int[] freq = new int[1001];
 		for (int i = 0; i < n; i ++) freq[readInt()] ++;
 		
+		int max = 0, maxCnt = 0;
 		for (int i = 1; i <= 1000; i ++) {
-			if (freq[i] != 0) arr.add(new Pair(i, freq[i]));
+			if (freq[i] > max) { max = freq[i]; maxCnt = 1; }
+			else if (freq[i] == max) maxCnt ++;
 		}
-		Collections.sort(arr);
-		
-		int max, secondMax;
-		if (arr.get(arr.size() - 1).val == arr.get(arr.size() - 2).val) { max = arr.get(arr.size() - 1).val; secondMax = max; }
-		else { max = arr.get(arr.size() - 1).val; secondMax = arr.get(arr.size() - 2).val; }
-	
-		ArrayList<Integer> m = new ArrayList<Integer>();
-		for (int i = 0; i < arr.size(); i ++) {
-			if (arr.get(i).val == max) m.add(arr.get(i).idx);
-		}
-		Collections.sort(m);
-		if (max == secondMax) System.out.println(m.get(m.size() - 1) - m.get(0));
-		else {
-			ArrayList<Integer> m2 = new ArrayList<Integer>();
-			for (int i = 0; i < arr.size(); i ++) if (arr.get(i).val == secondMax) m2.add(arr.get(i).idx);
-			Collections.sort(m2);
-			int ans = 0;
-			for (int i = 0; i < m.size(); i ++) ans = Math.max(ans, Math.max(Math.abs(m.get(i) - m2.get(0)), Math.abs(m.get(i) - m2.get(m2.size() - 1))));
+		if (maxCnt > 1) {
+			int lower = -1, upper = 0;
+			for (int i = 1; i <= 1000; i ++) {
+				if (lower == -1 && freq[i] == max) lower = i;
+				if (freq[i] == max) upper = i;
+			}
+			System.out.println(upper - lower);
+		} else {
+			int max2 = 0, ans = 0;
+			for (int i = 1; i <= 1000; i ++) if (freq[i] != max) max2 = Math.max(max2, freq[i]);
+			ArrayList<Integer> arr2 = new ArrayList<Integer>();
+			for (int i = 1; i <= 1000; i ++) if (freq[i] == max2) arr2.add(i);
+			for (int i = 1; i <= 1000; i ++) {
+				if (freq[i] == max) {
+					for (int j = 0; j < arr2.size(); j ++) ans = Math.max(ans, Math.abs(i - arr2.get(j)));
+				}
+			}
 			System.out.println(ans);
 		}
 	} 
