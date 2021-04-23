41d1e5be97f10307f6f1c7b64512b259116d5de4
Austin-X <32133198+Austin-X@users.noreply.github.com>
2021-04-23 10:42:05 -0400

    Create ECOO '21 Practice Contest - Tootsie Rolls!.java

---
+++ ECOO/ECOO '21 Practice Contest - Tootsie Rolls!.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class TootsieRolls {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int n = readInt();
+		
+		if (n < 3) System.out.println("Rocket!");
+		else if (n > 7) System.out.println("Tootsie Roll!");
+		else System.out.println("NO");
+	}
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) 
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine() throws IOException {
+		return br.readLine().trim();
+	}
+}
