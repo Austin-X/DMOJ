0f6af06e12f5fa5f6662a0f5958dce700bcb4b22
Austin-X <32133198+Austin-X@users.noreply.github.com>
2021-04-23 10:43:21 -0400

    Create ECOO '21 Practice Contest P2 - High Fiver!.java

---
+++ ECOO/ECOO '21 Practice Contest P2 - High Fiver!.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class HighFiver {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		int n = readInt();
+		
+		String str = String.valueOf(n);
+		if (n % 2 == 0 && n % 3 == 0 && str.charAt(str.length() - 1) != '2') System.out.println("not a memer");
+		else System.out.println("memer");
+	}
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) 
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine() throws IOException {
+		return br.readLine().trim();
+	}
+}
