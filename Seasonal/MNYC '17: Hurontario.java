585d18de71976ff0c379758e53a4b03c2e7d5372
Austin-X <32133198+Austin-X@users.noreply.github.com>
2021-08-15 20:39:29 -0400

    Update MNYC '17: Hurontario.java

--- Seasonal/MNYC '17: Hurontario.java
+++ Seasonal/MNYC '17: Hurontario.java
@@ -4,19 +4,27 @@ import java.util.*;
 public class Hurontario {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
+	
+	private static final int MOD = (int)(1e9) + 7;
 	public static void main(String[] args) throws IOException {
 		String a = next(), b = next();
-		int base = 131, idx = 0;
-		long hsh1 = 0, hsh2 = 0; long[] pow = new long[a.length()];
-		for (int i = a.length() - 1; i >= 0; i --) pow[i] = i == a.length() - 1 ? 1 : pow[i + 1] * base;
+		int base = 31, best = 0, mx = Math.max(a.length(), b.length());
+		long[] hshA = new long[a.length() + 1], hshB = new long[b.length() + 1], pow = new long[mx + 1];
+		pow[0] = 1;
+		for (int i = 1; i <= a.length(); i ++) hshA[i] = (hshA[i - 1] * base % MOD + a.charAt(i - 1) - 'A' + 1) % MOD;
+		for (int i = 1; i <= b.length(); i ++) hshB[i] = (hshB[i - 1] * base % MOD + b.charAt(i - 1) - 'A' + 1) % MOD;
+		for (int i = 1; i <= mx; i ++) pow[i] = pow[i - 1] * base % MOD;
 	
-		int best = a.length();
-		for (int i = a.length() - 1; i >= 0; i --) {
-			if (idx == b.length()) break;
-			hsh1 += a.charAt(i) * pow[i]; hsh2 = hsh2 * base + b.charAt(idx); idx ++;
+		for (int i = 1; i <= b.length(); i ++) {
+			if (i > a.length()) break;
+			long hsh1 = calcHsh(hshA, pow, a.length() - i + 1, a.length()), hsh2 = calcHsh(hshB, pow, 1, i);
 			if (hsh1 == hsh2) best = i;
 		}
-		System.out.print(a.substring(0, best)); System.out.println(b);
+		System.out.print(a.substring(0, a.length() - best)); System.out.println(b);
+	}
+	
+	static long calcHsh(long[] hsh, long[] pow, int x, int y) {
+		return (hsh[y] - hsh[x - 1] * pow[y - x + 1] % MOD + MOD) % MOD;
 	}
 
 	static String next() throws IOException {
