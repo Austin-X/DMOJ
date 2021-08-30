ceb35e5f7422fa4cf3225158304adaed7096b69d
Austin-X <32133198+Austin-X@users.noreply.github.com>
2021-08-30 12:53:27 -0400

    Create  Another Contest 8 Problem 2 - Unnecessary Trash Push.java

---
+++ Uncategorized/Another Contest 8 Problem 2 - Unnecessary Trash Push.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class UnnecessaryTrashPush {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	
+	public static void main(String[] args) throws IOException {
+		int t = readInt();
+		
+		while (t-- > 0) {
+			int n = readInt(), k = readInt(), trash = 0, ans = 0;
+			while (n-- > 0) {
+				int a = readInt();
+				if (trash + a >= k) { trash = 0; ans ++; }
+				else trash += a;
+			}
+			System.out.println(ans);
+		}
+	}
+
+	static String next() throws IOException {
+		while (st == null || !st.hasMoreTokens()) 
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine() throws IOException {
+		return br.readLine().trim();
+	}
+}
