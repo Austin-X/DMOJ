dad55af0f886b0852d66994c454bb7d88496fe49
FLAME FIRE <32133198+Austin-X@users.noreply.github.com>
2022-06-09 22:44:40 -0400

    Update SAC '22 Code Challenge 5 Junior P4 - Course Requirements.java

--- SAC Coding Challenge/SAC '22 Code Challenge 5 Junior P4 - Course Requirements.java
+++ SAC Coding Challenge/SAC '22 Code Challenge 5 Junior P4 - Course Requirements.java
@@ -12,27 +12,22 @@ public class CourseRequirements {
 		int[] freq = new int[n + 1];
 		ArrayList<Integer>[] adj = new ArrayList[n + 1];
 		for (int i = 1; i <= n; i ++) adj[i] = new ArrayList<Integer>();
-		
-		boolean[] vis = new boolean[n + 1];
+
 		Queue<Integer> q = new LinkedList<Integer>();
 		for (int i = 1; i <= n; i ++) {
-			int c = readInt();
-			freq[i] = c;
-			if (c == 0) { vis[i] = true; q.add(i); }
-			for (int j = 0; j < c; j ++) adj[readInt()].add(i);
+			freq[i] = readInt();
+			if (freq[i] == 0) q.add(i); 
+			for (int j = 0; j < freq[i]; j ++) adj[readInt()].add(i);
 		}
 		
-		int[] ans = new int[n];
-		int idx = 0;
 		while (!q.isEmpty()) {
 			int cur = q.poll();
-			ans[idx++] = cur;
+			System.out.print(cur + " ");
 			for (int x: adj[cur]) {
 				freq[x] --;
-				if (freq[x] == 0) { vis[x] = true; q.add(x); }
+				if (freq[x] == 0) q.add(x); 
 			}
 		}
-		for (int i = 0; i < n; i ++) System.out.print(i == n - 1 ? ans[i] + "\n" : ans[i] + " ");
 	}
 
 	static String next() throws IOException {
